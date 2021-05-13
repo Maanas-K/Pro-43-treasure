1df7730aa0508f958f3df5b8c8deb7d7e0563074
Maanas-K <76108537+Maanas-K@users.noreply.github.com>
2021-05-13 17:34:47 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,29 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+
+    fill("white")
+    text("A T E D A S B A",700,150)
+    fill("lightblue")
+    text("Hint: I am found in the clouds", 700, 170);
+
+    fill("white")
+    text("C U T N I F O N", 100, 280)
+    fill("lightblue")
+    text("Hint: I set up and draw", 100, 300)
+
+    fill("red")
+    textSize(20)
+    text("**please type answers in small letters**", 350, 450)
+    
+
+}
